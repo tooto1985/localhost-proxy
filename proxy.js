df1e8033f1f051ba2ead387d6ffc869aad6e3d50
Tommy <tooto1985@gmail.com>
2020-03-29 20:59:06 +0800

    modify

--- proxy.js
+++ proxy.js
@@ -1,23 +1,16 @@
-var ngrok = require('ngrok')
 var http = require('http')
 var httpProxy = require('http-proxy');
-
 (async function () {
-  var url = await ngrok.connect(3000)
-  console.log(url)
-
   var proxy = httpProxy.createProxyServer({
     headers: {
       host: 'localhost'
     }
   })
-
   http.createServer(function (request, response) {
     proxy.web(request, response, {
       target: 'http://localhost:2771'
     })
   }).listen(process.env.PORT || 3000)
-
   proxy.on('proxyRes', function (proxyRes, req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
